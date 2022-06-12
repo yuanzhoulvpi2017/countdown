5884aa72958bba337a905e82bd907fd72f624673
yuanz <yuanzhoulvpi@outlook.com>
2022-06-12 12:49:00 +0800

    update code

--- src/gen.rs
+++ src/gen.rs
@@ -1,6 +1,5 @@
 use image::{open, imageops};
 use std::collections::HashMap;
-
 #[warn(unused_assignments)]
 pub fn show_one2(number: &String) -> Box<Vec<String>> {
     let nwidth = 20;
@@ -90,7 +89,7 @@ pub fn show_label(number: &String) -> Box<Vec<String>> {
 
 
 pub struct CountDown {
-    simpleword: HashMap<&'static str, Box<Vec<String>>>,
+    simple_word: HashMap<&'static str, Box<Vec<String>>>,
 }
 
 impl CountDown {
@@ -98,21 +97,21 @@ impl CountDown {
         let mut dict_result = HashMap::new();
         let biglist = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", "day", "hou", "min", "sec"];
         let number = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":"];
-        for templist in biglist {
-            if number.contains(&templist) {
-                let result = show_one2(&templist.to_string());
-                dict_result.insert(templist, result);
+        for temp_list in biglist {
+            if number.contains(&temp_list) {
+                let result = show_one2(&temp_list.to_string());
+                dict_result.insert(temp_list, result);
             } else {
-                let result = show_label(&templist.to_string());
-                dict_result.insert(templist, result);
+                let result = show_label(&temp_list.to_string());
+                dict_result.insert(temp_list, result);
             }
         }
 
         return Self {
-            simpleword: dict_result,
+            simple_word: dict_result,
         };
     }
-    pub fn showtotallabel(&self) {
+    pub fn show_total_label(&self) {
         let temp_list = vec!["day", "hou", "min", "sec"];
 
 
@@ -123,7 +122,7 @@ impl CountDown {
 
         for tempvalue in temp_list {
             // let simple_vector = *show_label(&tempvalue.to_string());
-            let simple_vector = self.simpleword.get(tempvalue).unwrap();
+            let simple_vector = self.simple_word.get(tempvalue).unwrap();
 
 
             let mut index = 0;
@@ -156,7 +155,7 @@ impl CountDown {
 
         for tempvalue in temp_list {
             // let simple_vector = *show_one2(&tempvalue.to_string());
-            let simple_vector = self.simpleword.get(tempvalue).unwrap();
+            let simple_vector = self.simple_word.get(tempvalue).unwrap();
 
 
             let mut index = 0;
@@ -174,7 +173,7 @@ impl CountDown {
     pub fn beautifyshow(&self, day: i32, hour: i32, min: i32, sec: i32) {
 
         let datecollect = format!("{:02}:{:02}:{:02}:{:02}", day, hour, min, sec);
-        self.showtotallabel();
+        self.show_total_label();
         self.showtotaldate(&datecollect);
     }
 }
